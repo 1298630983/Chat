0e00c7d69fb979b65eafe0d9f4bc43086f3ae114
1298630983 <1298630983@qq.com>
2017-09-06 19:46:49 +0800

    Chat0.2

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -4,12 +4,19 @@ import java.awt.*;
  * Created by Jason on 2017/9/6.
  */
 public class ChatClient extends Frame{
+
+    TextField tfTxt = new TextField();
+    TextArea taContent = new TextArea();
+
     public static void main(String[] args) {
         new ChatClient().launchFrame();
     }
 
     public void launchFrame() {
         setBounds(100,100,400,400);
+        add(tfTxt,BorderLayout.SOUTH);
+        add(taContent,BorderLayout.NORTH);
+        pack();
         setVisible(true);
     }
 }

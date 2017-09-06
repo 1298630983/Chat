6a0cb8303bd36413bcd1128d34c7b4ee323b2424
1298630983 <1298630983@qq.com>
2017-09-06 20:22:40 +0800

    Chat0.3

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,4 +1,8 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by Jason on 2017/9/6.
@@ -16,7 +20,25 @@ public class ChatClient extends Frame{
         setBounds(100,100,400,400);
         add(tfTxt,BorderLayout.SOUTH);
         add(taContent,BorderLayout.NORTH);
+        tfTxt.addActionListener(new TfListener());
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                setVisible(false);
+                System.exit(0);
+            }
+        });
         pack();
         setVisible(true);
+    }
+
+    private class TfListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String s = tfTxt.getText().trim();
+            taContent.setText(s);
+            tfTxt.setText("");
+        }
     }
 }
